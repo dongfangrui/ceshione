9494a7dba01ae1683e3faaa3324147f495c4ee47
dongfangrui <13116813@qq.com>
2020-08-28 02:20:07 -0700

    删除测试

---
+++ .ccls-cache/@@home@xp@cceshixp@xppro/@home@xp@cceshixp@x.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+int main()
+{
+int i=0;
+for(i=0;i<10;i++)
+printf("%d\n",i);
+
+
+}
---
+++ .ccls-cache/@home@xp@cceshixp@xppro/.ccls-cache@@home@xp@cceshixp@xppro@cxp1@cxp1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include "../hxp/kh.h"
+
+void sort1()
+{
+    int i=0;
+    for(i=0;i<10;i++)
+    {
+        printf("from 1 to 10\n");
+        printf("%d\n",i);
+    }
+}
